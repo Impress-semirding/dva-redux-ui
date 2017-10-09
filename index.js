619e1a16776199d513979eecbcac004f114fb075
senir <dingxue@zhongan.io>
2017-10-09 10:09:48 +0800

    publish

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-/**
- * @author senir
- * @email 15251895266@163.com
- * @create date 2017-09-13 06:41:30
- * @modify date 2017-09-13 06:41:30
- * @desc [description]
-*/
-import ui from './ui';
-import reducer from './action-reducer.js';
+'use strict';
 
-
-function createUiState(opts = {}) {
-  // const namespace = opts.namespace || NAMESPACE;
-
-  const extraReducers = {
-    ['ui']: reducer,
-  };
-
-  function onEffect(effect, { put }, model, actionType) {
-    const { namespace } = model;
-    return function*(...args) {
-      yield put({ type: SHOW, payload: { namespace, actionType } });
-      yield effect(...args);
-      yield put({ type: HIDE, payload: { namespace, actionType } });
-    };
-  }
-
-  return {
-    extraReducers,
-    // onEffect,
-  };
-}
-
-export default {
-  createUiState,
-  ui,
-};
+module.exports = require('./lib');
---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * @author senir
+ * @email 15251895266@163.com
+ * @create date 2017-09-13 06:41:30
+ * @modify date 2017-09-13 06:41:30
+ * @desc [description]
+*/
+import ui from './ui';
+import reducer from './action-reducer.js';
+
+
+function createUiState(opts = {}) {
+  // const namespace = opts.namespace || NAMESPACE;
+
+  const extraReducers = {
+    ['ui']: reducer,
+  };
+
+  function onEffect(effect, { put }, model, actionType) {
+    const { namespace } = model;
+    return function*(...args) {
+      yield put({ type: SHOW, payload: { namespace, actionType } });
+      yield effect(...args);
+      yield put({ type: HIDE, payload: { namespace, actionType } });
+    };
+  }
+
+  return {
+    extraReducers,
+    // onEffect,
+  };
+}
+
+export default {
+  createUiState,
+  ui,
+};
